dd9d345dadd7af9458a17213263a83c0a98415d4
Litpax-Technology <mis@litpaxtechnology.com>
2026-07-02 11:36:08 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+// ============================================
+// LITPAX INITIATIVE TRACKER - CONFIG
+// GAS URL sirf yahan update karna hai
+// ============================================
+
+const CONFIG = {
+  GAS_URL: 'https://script.google.com/macros/s/AKfycbyzlYFZRnZ3H6xGXjLUiQyIlZJNl6e7FQ450kw17sBsujBfR_IT_lNzEpB4fvCS2eFH/exec'
+};
