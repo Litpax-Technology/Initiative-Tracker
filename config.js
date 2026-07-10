e16b0e8aefe8dc1b52e8a0b308690a85850e4d8b
Litpax-Technology <mis@litpaxtechnology.com>
2026-07-10 17:11:23 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,7 @@
 // ============================================
-// LITPAX INITIATIVE TRACKER - CONFIG
+// LITPAX - CONFIG  (Initiative Tracker + R&D Tracker dono isi ko use karte hain)
 // GAS URL sirf yahan update karna hai
 // ============================================
-
 const CONFIG = {
-  GAS_URL: 'https://script.google.com/macros/s/AKfycbwM7xTzilf41BRpAI1fVzaHNMMiuCyTEjSb7ZhxZLaJdWEBT8d0KwVhOoXE-x8rVqe6/exec'
+  GAS_URL: 'https://script.google.com/macros/s/AKfycbyaLVmMtlcEccuu64vXQN81Rd5COI8ZnNFFO4fxRBVC85p9YZIDumsMkPdZtA5RtXT0/exec'
 };
